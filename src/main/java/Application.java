3cdd3946fb841ed73752b65723e298b663b0a162
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-04-07 23:58:07 +0300

    constructor

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,7 +8,11 @@ public class Application {
         context.refresh();
 
         MusicPlayer musicPlayer = context.getBean("MusicPlayer", MusicPlayer.class);
+        Music music = context.getBean("musicBean2", Music.class);
+        MusicPlayer musicPlayer1= new MusicPlayer(music);
         musicPlayer.play();
+        System.out.print("constructor: ");
+        musicPlayer1.playMusic();
         context.close();
     }
 }
